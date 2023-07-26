f023542fec357f6008b4210aa40032b391bffe80
LH <70417546+LMileishkaH@users.noreply.github.com>
2023-07-26 19:53:23 -0400

    Add files via upload

---
+++ AffirmationGenerator.py
@@ -0,0 +1,99 @@
+#Title
+print("Affirmation Generator!!!")
+
+print()
+
+print("Welcome to the Affirmation Generator!")
+
+print()
+
+print("Before we give you your daily affirmation. I have to ask you a few questions")
+
+print()
+
+name = input("What is your name? ")
+  
+print()
+  
+docontinue = input("Do you still want your affirmation? (yes/no) ")
+  
+if docontinue == "yes":
+  print()
+      
+  print("Alrighty")
+    
+  print()
+  dayofweek = input("What is the day of week? ")
+    
+  if dayofweek == 'Sunday' or dayofweek == 'sunday':
+
+    print()
+    
+    print(name, "repeat after me")
+      
+    print()
+      
+    print("~I am proud of myself~")
+  
+  elif dayofweek == 'Monday' or dayofweek == 'monday':
+
+    print()
+    
+    print(name, "repeat after me")
+      
+    print()
+      
+    print("~I love myself for who I am!~")
+  
+  elif dayofweek == 'Tuesday' or dayofweek == 'tuesday':
+
+    print()
+      
+    print(name, "repeat after me")
+      
+    print()
+      
+    print("~I give up all self-attack and judgement!~")
+  
+  elif dayofweek == 'Wednesday' or dayofweek == 'wednesday':
+
+    print()
+      
+    print(name, "repeat after me")
+      
+    print()
+      
+    print("~I am willing to forgive those who hurt me!~")
+  
+  elif dayofweek == 'Thursday' or dayofweek == 'thursday':
+
+    print()
+  
+    print(name, "repeat after me")
+      
+    print()
+      
+    print("~Joy is my birthright!~")
+  
+  elif dayofweek == 'Friday' or dayofweek == 'friday':
+
+    print()
+  
+    print(name, "repeat after me")
+      
+    print()
+      
+    print("~I inspire those around me to live their truth!~")
+  
+  elif dayofweek == 'Saturday' or dayofweek == 'saturday':
+
+    print()
+    
+    print(name, "repeat after me")
+        
+    print()
+        
+    print("~I am confident in who I am!!")
+  
+else:
+  print("That's fine")
